5f946388aeff18231945a0ef39849de410f6cd70
user1 <user1@DESKTOP-8UC660Q>
2020-01-09 10:04:46 +0900

    회원 가입 관련 package, views 생성(join, idFind, passwordFind)

---
+++ ProjectArtree/src/main/java/masterpiece/exhibition/member/controller/MemberController.java
@@ -0,0 +1,29 @@
+package masterpiece.exhibition.member.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+	
+	@RequestMapping(value="/join.at")
+	public String join(HttpServletRequest request) {
+		
+		return "member/join/join.tiles";
+	} // end of join --------------------------------------------
+	
+	@RequestMapping(value="/idFind.at")
+	public String idFind(HttpServletRequest request) {
+		
+		return "member/login/idFind.tiles";
+	} // end of idFind --------------------------------------------
+	
+	@RequestMapping(value="/passwordFind.at")
+	public String passwordFind(HttpServletRequest request) {
+		
+		return "member/login/passwordFind.tiles";
+	} // end of passwordFind --------------------------------------------
+	
+}
