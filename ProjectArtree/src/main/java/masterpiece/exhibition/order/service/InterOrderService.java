7e669a6dfd0c8215b6e0ef26a59d8ce24a491e72
user1 <user1@DESKTOP-8UC660Q>
2020-01-22 09:11:07 +0900

    20200122 0911 habin

--- ProjectArtree/src/main/java/masterpiece/exhibition/order/service/InterOrderService.java
+++ ProjectArtree/src/main/java/masterpiece/exhibition/order/service/InterOrderService.java
@@ -7,4 +7,21 @@ public interface InterOrderService {
 
 	List<HashMap<String, String>> getEx(HashMap<String, String> map); // 전시회 정보 조회
 
+	int insertCart(HashMap<String, String> paraMap); // 장바구니 입력
+
+	String selectCartNo(String idx); // 장바구니 번호 불러오기
+
+	int insertCartDetail(HashMap<String, String> paraMap); // 장바구니 상세 입력
+
+	List<HashMap<String, String>> selectCartNoList(HashMap<String, String> map); // 장바구니 번호들 조회
+
+	List<HashMap<String, String>> cartDetailList(String string); // 상세 조회
+	
+	void delCartDetail(String cartNo); // 장바구니 상세 삭제
+	void delCart(String cartNo); // 장바구니 삭제
+
+
+	
+	
+
 }
