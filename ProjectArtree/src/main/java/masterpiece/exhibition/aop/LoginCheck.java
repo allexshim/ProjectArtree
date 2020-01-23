92a6e06f991444283676aa73c26d8c4a98f0ff4b
user1 <user1@DESKTOP-8UC660Q>
2020-01-23 17:47:53 +0900

    명절 잘 보내세요

--- ProjectArtree/src/main/java/masterpiece/exhibition/aop/LoginCheck.java
+++ ProjectArtree/src/main/java/masterpiece/exhibition/aop/LoginCheck.java
@@ -36,20 +36,24 @@ public class LoginCheck {
 			
 			if(session.getAttribute("loginuser") == null) {
 				try {
+					/*
+					String loc = "layer_open('layer')";
+					request.setAttribute("loc", loc);
+					
+					RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/msgForLogin.jsp");
+					
+					dispatcher.forward(request, response);*/
+					
 					String msg = "먼저 로그인하세요!"; 
 					String loc = "/artree";
 					
-					request.setAttribute("msg", msg);
+					request.setAttribute("msg", msg); 
 					request.setAttribute("loc", loc);
-					
-					// 로그인 한 후 로그인 하기 전 페이지로 돌아가는 작업 : goBackURL
-					// === 현재 페이지의 주소(URL) 알아내기 ===
-					// String url = MyUtil.getCurrentURL(request);
-					// session.setAttribute("gobackURL", url); // 세션에 url 정보를 저장시킨다.
 					
 					RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/msg.jsp");
 					
 					dispatcher.forward(request, response);
+					
 				} catch (ServletException | IOException e) {
 					e.printStackTrace();
 				} 
