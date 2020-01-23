1a5bfb43721d76925f645178900511908c6cfbb6
user1 <user1@DESKTOP-8UC660Q>
2020-01-23 15:18:16 +0900

    aop 에러 해결

--- ProjectArtree/src/main/java/masterpiece/exhibition/aop/LoginCheck.java
+++ ProjectArtree/src/main/java/masterpiece/exhibition/aop/LoginCheck.java
@@ -13,7 +13,6 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.ModelAndView;
 
 import masterpiece.exhibition.common.MyUtil;
 
@@ -22,12 +21,12 @@ import masterpiece.exhibition.common.MyUtil;
 public class LoginCheck {
 	/* requireLogin_ 으로 시작하는 모든 메소드 */
 	// === PointCut을 생성한다 === * Controller에서 해당 advice가 필요한 메소드 이름을 require기능_~으로 명명한다.
-		@Pointcut("execution(public * masterpiece.exhibition..*Controller.requireLogin_*(..))")
+		@Pointcut("execution(public * masterpiece.exhibition..*Controller.requireLogin_*(..)) ")
 		public void requireLogin() {}
 		
 		// === Before Advice를 구현한다 === //
 		@Before("requireLogin()")
-		public ModelAndView before(JoinPoint joinpoint, ModelAndView mav) {
+		public void before(JoinPoint joinpoint) {
 			// joinpoint : 포인트 컷 된 주업무의 method
 			
 			// 로그인 유무 확인 ~ 주업무 메소드의 parameter, request를 얻어온다.
@@ -40,9 +39,8 @@ public class LoginCheck {
 					String msg = "먼저 로그인하세요!"; 
 					String loc = request.getContextPath()+"/mainartree.at";
 					
-					mav.addObject("msg",msg);
-					mav.addObject("loc",loc);		
-					mav.setViewName("msg");
+					request.setAttribute("msg", msg);
+					request.setAttribute("loc", loc);
 					
 					// 로그인 한 후 로그인 하기 전 페이지로 돌아가는 작업 : goBackURL
 					// === 현재 페이지의 주소(URL) 알아내기 ===
@@ -56,6 +54,5 @@ public class LoginCheck {
 					e.printStackTrace();
 				} 
 			} // end of if ------------------------------------------------	
-			return mav;
 		} // end of before ----------------------------------------------------------
 }
--- ProjectArtree/src/main/java/masterpiece/exhibition/board/controller/BoardController.java
+++ ProjectArtree/src/main/java/masterpiece/exhibition/board/controller/BoardController.java
@@ -8,7 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BoardController {
 
-
+	@RequestMapping(value="/eventList.at")
+	public String eventList() {
+		return "board/events/eventList.tiles";
+	}
+	
+	@RequestMapping(value="/addEvent.at")
+	public String addEvent() {
+		
+		return "board/events/addEvent.tiles";
+	}
 	
 	
 	
