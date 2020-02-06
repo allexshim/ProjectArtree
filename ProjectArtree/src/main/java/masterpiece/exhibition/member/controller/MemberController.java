8d9ad20839f1f2cdc1e3394082cc1766281ca5a2
user1 <user1@DESKTOP-8UC660Q>
2020-02-06 12:45:39 +0900

    20200206

--- ProjectArtree/src/main/java/masterpiece/exhibition/member/controller/MemberController.java
+++ ProjectArtree/src/main/java/masterpiece/exhibition/member/controller/MemberController.java
@@ -11,6 +11,7 @@ import java.util.Random;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.json.JSONArray;
@@ -658,6 +659,8 @@ public class MemberController {
 			mav.setViewName("msg");
 		}
 				
+	    session.invalidate();
+		
 		return mav;
 	}
 	
@@ -707,5 +710,5 @@ public class MemberController {
 		return jsarr.toString();
 	
 	}
-	
+
 }
